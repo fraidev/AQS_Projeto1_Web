e9d5a479c62293d752fc093b36c1d9998d44a530
Frai <fraifelipe@gmail.com>
2019-05-27 19:45:37 -0300

    Checkpoint

--- src/main/java/domain/models/Bairro.java
+++ src/main/java/domain/models/Bairro.java
@@ -1,17 +1,12 @@
 package domain.models;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="tb_bairro")
@@ -27,10 +22,12 @@ public class Bairro  implements Serializable {
 	private List<Fiscalizacao> fiscalizacoes;
 	@OneToMany(mappedBy="bairro")
 	private List<Empresa> Empresas;
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JoinColumn(name="cidadeId")
 	private Cidade cidade;
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JoinColumn(name="ufId")
 	private Uf uf;
 	
